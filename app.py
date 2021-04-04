1739ffd84ce7029553d5d71016f400dc30bd44ca
Jaime Pascual Torres <jpascual@everlyrusher.com>
2021-04-04 21:49:05 +0200

    Random color, correct laser size

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, redirect, request, render_template, send_file
 from PIL import Image
 import face_recognition
 from io import BytesIO
+from math import sqrt
+import random
+
+# HELPERS
+# app.logger.info()
 
 # Get the centroids
 def centroid(vertexes):
@@ -12,11 +17,15 @@ def centroid(vertexes):
     _y = sum(_y_list) / _len
     return(_x, _y)
 
+def distance(A, B):
+    return sqrt( (A[0] - B[0])**2 + (A[1] - B[1])**2 )
+
 def serve_pil_image(pil_img, pil_format, pil_mimetype):
     img_io = BytesIO()
     pil_img.save(img_io, pil_format, quality=70)
     img_io.seek(0)
-    return send_file(img_io, mimetype=pil_mimetype)
+    return send_file(img_io, mimetype=pil_mimetype, as_attachment=True,
+            attachment_filename='laserized.' + str.lower(pil_format))
 
 app = Flask(__name__)
 
@@ -31,17 +40,34 @@ def index():
         # Find all facial features in all the faces in the image
         face_landmarks_list = face_recognition.face_landmarks(image)
 
+        # landmaks
+        A, B, C, D, E, F = face_landmarks_list[0]['right_eye']
+
         # Create a PIL imagedraw object so we can draw on the picture
         pil_image = Image.fromarray(image)
 
-        # Laser A
-        laserA = Image.open("LaserA.png")
+        # Laser Image
+        color = random.choice(['Blue', 'Yellow', 'Green', 'Orange'])
+        laserImage = Image.open("Laser"+color+".png")
+
+        # Centroids
         rightEyeCenter = centroid(face_landmarks_list[0]['right_eye'])
-        rightEyeCenterInt = (int(rightEyeCenter[0])-600,int(rightEyeCenter[1])-337)
-        pil_image.paste(laserA, rightEyeCenterInt, laserA)
         leftEyeCenter = centroid(face_landmarks_list[0]['left_eye'])
-        leftEyeCenterInt = (int(leftEyeCenter[0])-600,int(leftEyeCenter[1])-337)
-        pil_image.paste(laserA, leftEyeCenterInt, laserA)
+
+        # Resixe laser
+        distanceBetweenEyes = distance(rightEyeCenter, leftEyeCenter)
+        ratio = distance(A, D)/53 # 45 px para el LaserA
+
+        # Resize Laser image
+        laserImage = laserImage.resize((int(1200*ratio), int(674*ratio)))
+
+        # Right Laser
+        rightEyeCenterInt = (int(rightEyeCenter[0]-600*ratio),int(rightEyeCenter[1]-337*ratio))
+        pil_image.paste(laserImage, rightEyeCenterInt, laserImage)
+
+        # Left Laser
+        leftEyeCenterInt = (int(leftEyeCenter[0]-600*ratio),int(leftEyeCenter[1]-337*ratio))
+        pil_image.paste(laserImage, leftEyeCenterInt, laserImage)
 
         return serve_pil_image(pil_image, pilOrigFoto.format, pilOrigFoto.get_format_mimetype())
 
