d49ce1cf5e521ff2e5735709e7a4814482a50545
Jaime Pascual Torres <jpascual@everlyrusher.com>
2021-04-08 17:31:29 +0200

    Fix error with exif none

--- app.py
+++ app.py
@@ -37,12 +37,13 @@ def exifUp(image):
 
         exif = image._getexif()
 
-        if exif[orientation] == 3:
-            image=image.rotate(180, expand=True)
-        elif exif[orientation] == 6:
-            image=image.rotate(270, expand=True)
-        elif exif[orientation] == 8:
-            image=image.rotate(90, expand=True)
+        if exif is not None:
+            if exif[orientation] == 3:
+                image=image.rotate(180, expand=True)
+            elif exif[orientation] == 6:
+                image=image.rotate(270, expand=True)
+            elif exif[orientation] == 8:
+                image=image.rotate(90, expand=True)
 
     except (AttributeError, KeyError, IndexError):
         # cases: image don't have getexif
